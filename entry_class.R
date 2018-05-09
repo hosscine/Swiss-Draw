cd866f4f6ae9aad9fb8209bd6acf633a84ee77a0
hosscine <futagamirentaro@gmail.com>
2018-05-10 00:10:14 +0900

    userをplayerにリネーム

--- entry_class.R
+++ entry_class.R
@@ -1,8 +1,8 @@
 
-loadEntryData <- function(file){
+loadEntryData <- function(file = "sample2.entry"){
   load(file)
-  ent <- entryData$new(deck.names = as.character(deck$deck.name), deck.user.id = deck$member.id,
-                       user.names = member)
+  ent <- entryData$new(deck.names = as.character(deck$deck.name), deck.player.id = deck$member.id,
+                       player.names = member)
   return(ent)
 }
 
@@ -13,10 +13,10 @@ entryData <- R6::R6Class(
   # public field ------------------------------------------------------------
 
   public = list(
-    initialize = function(deck.names, deck.user.id, user.names){
+    initialize = function(deck.names, deck.player.id, player.names){
       private$decks <- deck.names
-      private$deck.user.id <- deck.user.id
-      private$users <- user.names
+      private$decks.player.id <- deck.player.id
+      private$players <- player.names
     }
   ),
 
@@ -24,16 +24,25 @@ entryData <- R6::R6Class(
 
   private = list(
     decks = NULL,
-    deck.user.id = NULL,
+    decks.player.id = NULL,
     
-    users = NULL
+    players = NULL
   ),
 
   # active binding-----------------------------------------------------------
 
   active = list(
-    deck = function() return(private$decks),
-    user = function() return(private$users),
-    deck.user = function() return(private$users[private$deck.user.id])
+    ndeck = function() private$decks %>% length,
+    nplayer = function() private$players %>% length,
+    
+    deck = function() private$decks,
+    
+    deck.player.id = function(){
+      ret <- private$decks.player.id; names(ret) <- self$deck; return(ret)},
+    
+    player = function() private$players,
+    
+    deck.player = function(){
+      ret <- private$players[private$decks.player.id]; names(ret) <- self$deck; return(ret)}
   )
 )
