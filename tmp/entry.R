fc708d39f2b9338f88ca0cb041af54919aab9458
hosscine <futagamirentaro@gmail.com>
2018-05-15 20:58:38 +0900

    いらないファイルを削除

--- tmp/entry.R
+++
@@ -1,6 +0,0 @@
-member <- c("A","B","C","D","E","F")
-
-deck <- data.frame(
-  deck.name = c("M・HERO","Rank4・HERO","カオスソルジャー","左腕芝刈ノイド",
-         "水晶機巧","SR","バージェストマ","kozmo","幻奏マドルチェ","月光","ABC","青眼"),
-  member.id = c(1,1,2,2,3,3,4,4,5,5,6,6))
